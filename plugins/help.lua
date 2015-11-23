f98ffbbba6dc7f89fbdf0ed9565fd25be11cd953
mamaddeveloper <mr.ashkan7786@gmail.com>
2015-11-23 19:42:05 +0330

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -111,6 +111,7 @@ local function telegram_help()
   text = text..'\n'..'There are '..i..' plugins help available.'
   text = text..'\n'..'Write "!help [plugin name]" or "!help [plugin number]" for more info.'
   text = text..'\n'..'Or "!help all" to show all info.'
+  text = text..'\n'..'Owner Bot is: @GenerousMan_Bot :)'
   return text
 end
  
